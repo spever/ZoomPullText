9149b66b4cc09eb71118dfb8d31b597dcd5728d3
spever <spever@126.com>
2017-02-14 13:22:48 +0800

    tinker 热修复

--- app/src/main/java/com/example/luo/zoompulltext/MainActivity.java
+++ app/src/main/java/com/example/luo/zoompulltext/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.luo.zoompulltext;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.luo.zoompulltext.util.TinkerManager;
+import com.example.luo.zoompulltext.util.Utils;
+import com.tencent.tinker.lib.service.TinkerPatchService;
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+import com.tencent.tinker.lib.util.TinkerServiceInternals;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvTextView = (TextView) findViewById(R.id.tv_text);
+        tvTextView.setText("我是patch");
+        tvTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TinkerInstaller.onReceiveUpgradePatch(getApplicationContext(), Environment.getExternalStorageDirectory().getAbsolutePath() + "/patch_signed_7zip.apk");
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/luo/zoompulltext/MyApplicationLike.java
@@ -0,0 +1,53 @@
+package com.example.luo.zoompulltext;
+
+import android.annotation.TargetApi;
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.multidex.MultiDex;
+
+import com.example.luo.zoompulltext.util.SampleApplicationContext;
+import com.example.luo.zoompulltext.util.TinkerManager;
+import com.tencent.tinker.anno.DefaultLifeCycle;
+import com.tencent.tinker.lib.tinker.Tinker;
+import com.tencent.tinker.loader.app.DefaultApplicationLike;
+import com.tencent.tinker.loader.shareutil.ShareConstants;
+
+/**
+ * Created by luo
+ * on 17/2/10.
+ */
+@SuppressWarnings("unused")
+@DefaultLifeCycle(application = "com.example.luo.zoompulltext.MyApplication",
+        flags = ShareConstants.TINKER_ENABLE_ALL,
+        loadVerifyFlag = false)
+public class MyApplicationLike extends DefaultApplicationLike {
+    public MyApplicationLike(Application application, int tinkerFlags, boolean tinkerLoadVerifyFlag, long applicationStartElapsedTime, long applicationStartMillisTime, Intent tinkerResultIntent) {
+        super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime, applicationStartMillisTime, tinkerResultIntent);
+    }
+
+    @Override
+    public void onBaseContextAttached(Context base) {
+        super.onBaseContextAttached(base);
+        MultiDex.install(base);
+
+        SampleApplicationContext.application = getApplication();
+        SampleApplicationContext.context = getApplication();
+        TinkerManager.setTinkerApplicationLike(this);
+
+        TinkerManager.initFastCrashProtect();
+        //should set before tinker is installed
+        TinkerManager.setUpgradeRetryEnable(true);
+
+        TinkerManager.installTinker(this);
+        Tinker tinker = Tinker.with(getApplication());
+
+
+    }
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    public void registerActivityLifecycleCallbacks(Application.ActivityLifecycleCallbacks callback) {
+        getApplication().registerActivityLifecycleCallbacks(callback);
+    }
+
+}
